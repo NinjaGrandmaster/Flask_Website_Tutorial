fea3ed88d858a3254cd2eccfadeeb2091c30c483
raren003 <raren003@ucr.edu>
2020-04-15 23:43:52 -0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 # "config" is the name of the Python module config.py, and obviously the one with the uppercase "C" is the actual class.
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)  # initialize database
+migrate = Migrate(app, db)  # initialize migration engine
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # get datebase url from environment variables
+    # as fallback configure database app.db in application main directory
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATA_BASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Disable signal that signals application every time a database change is made
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
